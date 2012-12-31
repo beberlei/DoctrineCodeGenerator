8b4e620225214b0a2f77f001e18add3edfaa9e70
Benjamin Eberlei <kontakt@beberlei.de>
2012-12-31 12:10:57 +0100

    Fix bug with Method params

--- lib/Doctrine/CodeGenerator/Builder/MethodBuilder.php
+++ lib/Doctrine/CodeGenerator/Builder/MethodBuilder.php
@@ -27,7 +27,7 @@ class MethodBuilder extends AbstractBuilder
 {
     private $builder;
     private $class;
-    private $params;
+    private $params = array();
 
     public function __construct($name, ClassBuilder $class)
     {
@@ -69,7 +69,7 @@ class MethodBuilder extends AbstractBuilder
     public function getNode()
     {
         $node = $this->builder->getNode();
-        //$node->params = $this->params;
+        $node->params = $this->params;
         return $node;
     }
 }
--- sandbox/dumper.php
+++ sandbox/dumper.php
@@ -2,7 +2,12 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$code = "<?php\n" . $argv[1];
+if ( file_exists($argv[1])) {
+    $code = file_get_contents($argv[1]);
+} else {
+    $code = "<?php\n" . $argv[1];
+}
+
 $parser = new PHPParser_Parser(new PHPParser_Lexer);
 $stmts = $parser->parse($code);
 
